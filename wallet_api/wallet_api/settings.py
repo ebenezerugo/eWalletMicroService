864951daa7939f449a6dc823a1c249ea54131a8c
Rahul Pugal <rahul.pugal@mountblue.io>
2018-07-17 19:37:50 +0530

    swiched databse from sqlite to mysql

--- wallet_api/wallet_api/settings.py
+++ wallet_api/wallet_api/settings.py
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'wallet_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ewallet',
+        'USER': 'rahul',
+        'PASSWORD': 'password',
+        }
 }
 
 
